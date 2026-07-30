8ece7270fd48226230163a8905a005bbcc84da93
mdmufaz <mdmufaz58@gmail.com>
2026-07-31 01:15:33 +0530

    adedd the vite ap urll in the signupa

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function SignUp() {
   const navigate = useNavigate();
